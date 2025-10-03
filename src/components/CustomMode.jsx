ef5901e05401d1078b263b1bdb9cc5d1ca1a5c68
Talan Knipp <talanknipp@gmail.com>
2025-10-02 19:39:43 -0500

    fixed o formation and set up player card

--- src/components/CustomMode.jsx
+++ src/components/CustomMode.jsx
@@ -3,24 +3,24 @@ import "./CustomMode.css";
 
 const formations = {
   offense: [
-    { id: "QB", type: "QB", top: "70%", left: "45%" },
-    { id: "RB", type: "RB", top: "60%", left: "45%" },
-    { id: "FB", type: "FB", top: "65%", left: "55%" },
-    { id: "WR1", type: "WR", top: "50%", left: "10%" },
-    { id: "WR2", type: "WR", top: "50%", left: "80%" },
-    { id: "SWR", type: "WR", top: "55%", left: "65%" },
-    { id: "TE", type: "TE", top: "55%", left: "30%" },
-    { id: "LT", type: "OL", top: "40%", left: "25%" },
-    { id: "LG", type: "OL", top: "40%", left: "35%" },
-    { id: "C", type: "OL", top: "40%", left: "45%" },
-    { id: "RG", type: "OL", top: "40%", left: "55%" },
-    { id: "RT", type: "OL", top: "40%", left: "65%" },
+    { id: "QB", type: "QB", top: "45%", left: "45%" },
+    { id: "RB", type: "RB", top: "62%", left: "52%" },
+    { id: "FB", type: "FB", top: "62%", left: "38%" },
+    { id: "WR1", type: "WR", top: "10%", left: "2%" },
+    { id: "WR2", type: "WR", top: "12%", left: "92%" },
+    { id: "SWR", type: "WR", top: "15%", left: "14%" },
+    { id: "TE", type: "TE", top: "15%", left: "75%" },
+    { id: "LT", type: "LT", top: "20%", left: "25%" },
+    { id: "LG", type: "LG", top: "15%", left: "35%" },
+    { id: "C", type: "C", top: "10%", left: "45%" },
+    { id: "RG", type: "RG", top: "15%", left: "55%" },
+    { id: "RT", type: "RT", top: "20%", left: "65%" },
   ],
   defense: [
-    { id: "DL1", type: "DL", top: "40%", left: "25%" },
-    { id: "DL2", type: "DL", top: "40%", left: "40%" },
-    { id: "DL3", type: "DL", top: "40%", left: "55%" },
-    { id: "DL4", type: "DL", top: "40%", left: "70%" },
+    { id: "DE1", type: "DE", top: "40%", left: "25%" },
+    { id: "DT1", type: "DT", top: "40%", left: "40%" },
+    { id: "DT2", type: "DT", top: "40%", left: "55%" },
+    { id: "DE2", type: "DE", top: "40%", left: "70%" },
     { id: "LB1", type: "LB", top: "55%", left: "35%" },
     { id: "LB2", type: "LB", top: "55%", left: "50%" },
     { id: "LB3", type: "LB", top: "55%", left: "65%" },
@@ -36,25 +36,16 @@ const formations = {
   ],
 };
 
-// Simple player list for testing
+// Example players w/ images
 const availablePlayers = {
-  QB: [{ id: 1, name: "Patrick Mahomes" }, { id: 2, name: "Josh Allen" }],
-  RB: [{ id: 3, name: "Derrick Henry" }, { id: 4, name: "Christian McCaffrey" }],
-  FB: [{ id: 5, name: "Kyle Juszczyk" }],
-  WR: [
-    { id: 6, name: "Davante Adams" },
-    { id: 7, name: "Tyreek Hill" },
-    { id: 8, name: "Stefon Diggs" },
+  QB: [
+    { id: 1, name: "Patrick Mahomes", jnum: 15, image: "/images/mahomes.png" },
+    { id: 2, name: "Josh Allen", jnum: 17, image: "/images/allen.png" },
   ],
-  TE: [{ id: 9, name: "Travis Kelce" }, { id: 10, name: "Mark Andrews" }],
-  OL: [{ id: 11, name: "Trent Williams" }, { id: 12, name: "David Bakhtiari" }],
-  DL: [{ id: 13, name: "Aaron Donald" }, { id: 14, name: "Myles Garrett" }],
-  LB: [{ id: 15, name: "Fred Warner" }, { id: 16, name: "Darius Leonard" }],
-  CB: [{ id: 17, name: "Jalen Ramsey" }, { id: 18, name: "Jaire Alexander" }],
-  S: [{ id: 19, name: "Minkah Fitzpatrick" }, { id: 20, name: "Derwin James" }],
-  K: [{ id: 21, name: "Justin Tucker" }],
-  P: [{ id: 22, name: "Johnny Hekker" }],
-  LS: [{ id: 23, name: "Josh Harris" }],
+  WR: [
+    { id: 3, name: "Justin Jefferson", jnum: 18, image: "/images/jefferson.png" },
+    { id: 4, name: "Tyreek Hill", jnum: 10, image: "/images/hill.png" },
+  ],
 };
 
 const CustomMode = () => {
@@ -83,7 +74,7 @@ const CustomMode = () => {
     setSelectedPlayer(null);
   };
 
-  // Filter available players to prevent duplicates
+  // Prevent duplicate assignment
   const getFilteredPlayers = (type) => {
     const usedIds = Object.values(assigned).map((p) => p?.id);
     return availablePlayers[type]?.filter((p) => !usedIds.includes(p.id)) || [];
@@ -114,7 +105,15 @@ const CustomMode = () => {
               style={{ top: slot.top, left: slot.left }}
               onClick={() => handleSlotClick(slot)}
             >
-              {assigned[slot.id]?.name || slot.type}
+              {assigned[slot.id]?.image ? (
+                <img
+                  src={assigned[slot.id].image}
+                  alt={assigned[slot.id].name}
+                  style={{ width: "100%", height: "100%", borderRadius: "5px" }}
+                />
+              ) : (
+                slot.type
+              )}
             </div>
           ))}
         </div>
@@ -138,7 +137,15 @@ const CustomMode = () => {
                 }`}
                 onClick={() => handlePlayerSelect(player)}
               >
-                {player.name}
+                <img
+                  src={player.image}
+                  alt={player.name}
+                  className="player-img"
+                />
+                <div className="player-info">
+                  <h3>{player.name}</h3>
+                  <p>#{player.jnum}</p>
+                </div>
               </div>
             ))}
           </div>
